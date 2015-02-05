aa5f3d4f51c6bff092b44ff916f177fc328a1456
matiboy <mathieu.chauvinc@gmail.com>
2015-02-05 12:53:00 +0800

    Fix name for movieList

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('moviemaniaApp')
   .controller('MainCtrl', function ($scope) {
-    var movies = [{
+    var movieList = [{
       title: 'Usual Suspects',
       image: 'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_SX214_AL_.jpg',
       description: 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.'
