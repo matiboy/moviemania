ebab6c514bea925a7818a7ba3018a34b284e4ee3
matiboy <mathieu.chauvinc@gmail.com>
2015-02-05 12:27:24 +0800

    Define the controller as an extension to the app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moviemaniaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moviemaniaApp
+ */
+angular.module('moviemaniaApp')
+  .controller('MainCtrl', function ($scope) {
+    console.debug('This page uses MainCtrl');
+  });
