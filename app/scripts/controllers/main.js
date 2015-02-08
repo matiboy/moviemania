07d31451dc9df94589c1b1b0e4ddd2b93d683811
matiboy <mathieu.chauvinc@gmail.com>
2015-02-09 06:15:13 +0800

    Prepare form for section 3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,14 +12,15 @@ angular.module('moviemaniaApp')
     var movieList = [{
       title: 'Usual Suspects',
       image: 'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_SX214_AL_.jpg',
-      description: 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.'
+      description: 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.',
+      category: 'Awesome'
     },
     {
       title: 'Star Wars',
       image: 'http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX214_AL_.jpg',
-      description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader."
+      description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
+      category: 'Epic'
     }];
 
     $scope.movies = movieList;
-    $scope.invalidBinding = 'I WILL NOT APPEAR ON THE PAGE...';
   });
