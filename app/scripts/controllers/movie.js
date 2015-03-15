827796a43d9c14e34d2f437b985a72421e243e93
matiboy <mathieu.chauvinc@gmail.com>
2015-03-15 11:45:20 +0800

    No need +

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -6,7 +6,7 @@ angular.module('moviemaniaApp')
     var movieList = [{
       id: 23,
       title: 'Usual Suspects',
-      searchTerms: 'the+usual+suspects',
+      searchTerms: 'the usual suspects',
       image: 'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_SX214_AL_.jpg',
       description: 'A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.',
       category: 'Awesome',
@@ -19,7 +19,7 @@ angular.module('moviemaniaApp')
     {
       id: 42,
       title: 'Star Wars',
-      searchTerms: 'star+wars+1977',
+      searchTerms: 'star wars 1977',
       image: 'http://ia.media-imdb.com/images/M/MV5BMTU4NTczODkwM15BMl5BanBnXkFtZTcwMzEyMTIyMw@@._V1_SX214_AL_.jpg',
       description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
       category: 'Epic',
@@ -45,6 +45,20 @@ angular.module('moviemaniaApp')
       var movie = movieList[index];
       if(movie.id === id) {
         $scope.movie = movie;
+        $http.jsonp('http://api.duckduckgo.com', {
+          params: {
+            q: $scope.movie.searchTerms,
+            format: 'json',
+            pretty: 1,
+            callback: 'JSON_CALLBACK'
+          }
+        }).success(function(data) {
+          console.log(data);
+          $scope.movie.abstract = data.AbstractText;
+          $scope.movie.abstractSource = data.AbstractSource;
+        }).error(function(){
+          console.log(arguments);
+        });
       }
     }
   });
