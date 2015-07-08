6a14b648edf075f33584b0783122a9fc36adf2c0
matiboy <mathieu.chauvinc@gmail.com>
2015-07-08 15:49:34 +0800

    Add short delay to demo the loader

--- scripts/controllers/movie.js
+++ scripts/controllers/movie.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('moviemaniaApp')
-  .controller('MovieCtrl', ['$scope', '$routeParams', 'Movies', function ($scope, $routeParams, Movies) {
+  .controller('MovieCtrl', ['$scope', '$routeParams', 'Movies', function ($scope, $routeParams, Movies, $timeout) {
     var id = parseInt($routeParams.id, 10);
     Movies.get(id).then(function(movie) {
       $scope.movie = movie;
       // Load duckgogo
       $scope.loading = true;
       Movies.moreInfo(movie).then(function(info) {
-        $scope.loading = false;
-        $scope.movie.abstract = info.abstract;
-        $scope.movie.abstractSource = info.abstractSource;
+        $timeout(function() {
+          $scope.loading = false;
+          $scope.movie.abstract = info.abstract;
+          $scope.movie.abstractSource = info.abstractSource;
+        }, 1200);
       });
     });
   }]);
